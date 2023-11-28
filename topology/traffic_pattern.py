5e8479157ea1abd88aab9954457934ba46642d5c
pietrolechthaler <pietrolechthaler@gmail.com>
2023-11-28 16:28:38 +0100

    added some comments

--- topology/traffic_pattern.py
+++ topology/traffic_pattern.py
@@ -10,6 +10,15 @@ from subprocess import Popen
 import time
 import random
 
+# ---------------------------------------------------------------- #
+# -------------------------- Parameters -------------------------- #
+# ---------------------------------------------------------------- #
+SWITCHES = 5
+HOSTS = 3
+CROSS_CONNECTION = .3 #30% chance of having cross connections
+IDLE_PERCENT = .05 #probability of a host remaining idle
+NUM_ITERATIONS = 512
+
 class arbitrary_topology(Topo):
     def build(self, total_switches, max_hosts_per_switch, interconnectivity, seed=0):
         global host_count
@@ -20,6 +29,7 @@ class arbitrary_topology(Topo):
         for i in range(total_switches):
             switch_dpid = f"s{i+1}"
             self.addSwitch(switch_dpid, stp=True, failMode='standalone')    #stp to avoid loops in the networks 
+            
             #for each switch create and connect all the hosts
             for j in range(random.randrange(max_hosts_per_switch)):
                 host_dpid = f"h{host_count+1}"
@@ -50,17 +60,22 @@ class arbitrary_topology(Topo):
 
 
 if __name__ == "__main__":
-    info('*** Clean net\n')
-    cmd = "mn -c"                      #run sudo mn -c to clear mininet
-    Popen(cmd, shell=True).wait()
-    
-    idle_percent = .05
-    NUM_ITERATIONS = 512
-    topo = arbitrary_topology(5,3,.3,0)     #crete a network with 5 switches, each with 0-3 hosts and a 30% chance of having cross connections
 
-    setLogLevel( 'info' )
+    #clean mininet istances
+    info('*** Clean net\n')
+    Popen("mn -c", shell=True).wait()
+
+    #topology creation
+    topo = arbitrary_topology(SWITCHES,HOSTS,CROSS_CONNECTION,0)
+
+    #Logging
+    setLogLevel('info')
+
+    #Controller
     controller = RemoteController("c0", ip="0.0.0.0", port=6633) 
-    net = Mininet(                                                #configure miniset, autoatically set mac and ip addresses 
+    
+    #Mininet set-up (mac and ip auto-set)
+    net = Mininet(                                                
         topo=topo,
         switch=OVSKernelSwitch,
         build=False,
@@ -73,10 +88,8 @@ if __name__ == "__main__":
     net.build()     
     net.start()
 
-
     print("\n\n--------------------------------------------------------------------------------")    
     print("Network built, waiting for STP to configure itself\n\n")    
-
 
     s1 = net.get("s1")   #check he state stp, we wait until s1 says "forward" which indicates it is complete
     while(s1.cmdPrint('ovs-ofctl show s1 | grep -o FORWARD | head -n1') != "FORWARD\r\n"): 
@@ -90,16 +103,16 @@ if __name__ == "__main__":
     print("\n\n--------------------------------------------------------------------------------")    
     print("Begin traffic generation\n\n")    
     
-    
     random.seed(0)
 
-    #gather hosts in an easy-to-access array
+    #Hosts array
     hs = [net.get(f"h{host+1}") for host in range(host_count)]
     
-    #start a http server on host h1
+    #Start a HTTP server on Host h1
     http_server = random.choice(hs)
     http_server.cmd('python2 -m SimpleHTTPServer 80 &')
 
+    #Start iperf (TCP and UDP) server on Random Hosts
     iperf_TCP_server = random.choice(hs)
     iperf_TCP_server.cmd('iperf -s -p 5050 &') 
     
@@ -107,11 +120,10 @@ if __name__ == "__main__":
     iperf_UDP_server.cmd('iperf -s -u -p 5051 &')
 
 
-
     end_time = time.time() + 30  
     while time.time() < end_time:  #do this for 30 seconds
         for j,h in enumerate(hs):
-            if random.random() < idle_percent:  #sometimes the client doesn't do anything
+            if random.random() < IDLE_PERCENT:  #sometimes the client doesn't do anything
                 continue
             actions = [ lambda: h.cmd(f"iperf -p 5050 -t 1 -c {iperf_TCP_server.IP()}"),      #iperf server TCP
                         lambda: h.cmd(f"iperf -p 5051 -t 1 -u -c {iperf_UDP_server.IP()}"),   #iperf random host UDP 
