51b762aea71ab84fce637b81f9d2458445a1c27b
guss-alberto <96133902+guss-alberto@users.noreply.github.com>
2023-11-29 19:42:54 +0100

    clean up net generation code

--- topology/traffic_pattern.py
+++ topology/traffic_pattern.py
@@ -15,48 +15,49 @@ import random
 # ---------------------------------------------------------------- #
 SWITCHES = 5
 HOSTS = 3
-CROSS_CONNECTION = .3 #30% chance of having cross connections
+CROSS_CONNECTION = .30 #30% chance of having cross connections
 IDLE_PERCENT = .05 #probability of a host remaining idle
-NUM_ITERATIONS = 512
+TEST_TIME = 30 #test time in seconds
 
 class arbitrary_topology(Topo):
     def build(self, total_switches, max_hosts_per_switch, interconnectivity, seed=0):
         global host_count
         random.seed(seed)
 
-        host_count =0 
+        host_count = 0 
         #create all the switces and hosts
         for i in range(total_switches):
             switch_dpid = f"s{i+1}"
             self.addSwitch(switch_dpid, stp=True, failMode='standalone')    #stp to avoid loops in the networks 
-            
+
+            if i > 0:
+                self.addLink(switch_dpid, f"s{i}")      #add link to previous switch in a line topology
+
             #for each switch create and connect all the hosts
             for j in range(random.randrange(max_hosts_per_switch)):
                 host_dpid = f"h{host_count+1}"
                 self.addHost(host_dpid)
                 self.addLink(switch_dpid, host_dpid)
                 host_count +=1
-        
+    
+
         # add connections between switches
-        for i in range(total_switches):
-            for j in range(total_switches):
-                # you can't connect to yourself 
-                if j == i:
+        connected_pairs = set()
+        for i in range(1, total_switches + 1):
+            for j in range(1, total_switches + 1):
+                # do not link with yourself 
+                if i == j:
+                    continue
+                # already linked with previous router 
+                if i == j + 1 or i == j - 1:
+                    continue
+                # check if the reverse connection already exists
+                if (j, i) in connected_pairs:
                     continue
 
-                # j == i+1 ensures a line topology connection through all the hosts
-                # we then use the interconnectivity percentage to add extra links in a random way
-                if j == i+1 or random.random() < interconnectivity:
-                    switch1_dpid = f"s{i+1}"
-                    switch2_dpid = f"s{j+1}"
-                    try:
-                        #check if reverse link already exists
-                        #garbage hack but it works
-                        self.linkInfo(switch2_dpid,switch1_dpid)
-                        print(f"Link {switch1_dpid} <-> {switch2_dpid} Already exists")
-                    except:
-                        self.addLink(switch1_dpid, switch2_dpid)
-
+                if random.random() < interconnectivity:
+                    connected_pairs.add((i, j))  # Add the connected pair to the set
+                    self.addLink(f"s{i}", f"s{j}")
 
 
 if __name__ == "__main__":
@@ -88,6 +89,9 @@ if __name__ == "__main__":
     net.build()     
     net.start()
 
+    CLI(net)    #once done open the minent Command Line Interface for testing before exiting
+    net.stop()
+
     print("\n\n--------------------------------------------------------------------------------")    
     print("Network built, waiting for STP to configure itself\n\n")    
 
@@ -105,9 +109,11 @@ if __name__ == "__main__":
     
     random.seed(0)
 
+
     #Hosts array
-    hs = [net.get(f"h{host+1}") for host in range(host_count)]
-    
+    hs = [net.get(f"h{host}") for host in range(1,host_count+1)]
+
+
     #Start a HTTP server on Host h1
     http_server = random.choice(hs)
     http_server.cmd('python2 -m SimpleHTTPServer 80 &')
@@ -120,9 +126,9 @@ if __name__ == "__main__":
     iperf_UDP_server.cmd('iperf -s -u -p 5051 &')
 
 
-    end_time = time.time() + 30  
+    end_time = time.time() + TEST_TIME  
     while time.time() < end_time:  #do this for 30 seconds
-        for j,h in enumerate(hs):
+        for h in hs:
             if random.random() < IDLE_PERCENT:  #sometimes the client doesn't do anything
                 continue
             actions = [ lambda: h.cmd(f"iperf -p 5050 -t 1 -c {iperf_TCP_server.IP()}"),      #iperf server TCP
