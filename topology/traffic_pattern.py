95d2c3f0ef6f63e475e78ec48176d4e03f87d72d
pietrolechthaler <pietrolechthaler@gmail.com>
2024-04-15 21:04:59 +0200

    structure refactor

--- topology/traffic_pattern.py
+++ topology/traffic_pattern.py
@@ -11,180 +11,203 @@ import time
 import re
 import random
 import os
-import urllib.parse
+import networkx as nx
+import matplotlib.pyplot as plt
+import argparse
 
-# ---------------------------------------------------------------- #
-# -------------------------- Parameters -------------------------- #
-# ---------------------------------------------------------------- #
+#Simulation parameters
+TEST_TIME = 30 
+folder_captures = "captures" 
 
-SWITCHES = 7              #number of switches, literal
-HOSTS = 2                 #max number of hosts PER EACH SWITCH
-CROSS_CONNECTION = .30    #30% chance of having cross connections
-
-
-IDLE_PERCENT = .2         #probability of a host remaining idle
-TEST_TIME = 30            #test time in seconds
+#Connection parameters
 IPERF_TIME_TCP = 2
 IPERF_TIME_UDP = 0.2
+NUM_IPERF_FLOWS = 3
 HOST_LINK_MAX_BW = 2
 HOST_LINK_MIN_BW = 1
 SWITCH_LINK_MAX_BW = 2
 SWITCH_LINK_MIN_BW = 1
 
-folder_path = "captures"  #folder to temporarily store the .pcap captures in 
+class Topology(Topo):
+    def __init__(self, num_switches, num_hosts, interconnectivity, seed = 0):
+            super().__init__()
+            self.num_switches = num_switches
+            self.num_hosts = num_hosts
+            self.interconnectivity = interconnectivity
+            self.seed = seed
 
-NUM_BASE_FLOWS = 3 #number of iperf flows in the background
+            global host_count
+            random.seed(self.seed)
+            host_count = 0 
+            #create all the switces and hosts
+            for i in range(self.num_switches):
+                switch_dpid = f"s{i+1}"
+                self.addSwitch(switch_dpid, stp=True, failMode='standalone')    #stp to avoid loops in the networks 
 
-class arbitrary_topology(Topo):
-    def build(self, total_switches, max_hosts_per_switch, interconnectivity, seed=0):
-        global host_count
-        random.seed(seed)
+                if i > 0:
+                    self.addLink(switch_dpid, f"s{i}")      #add link to previous switch in a line topology
 
-        host_count = 0 
-        #create all the switces and hosts
-        for i in range(total_switches):
-            switch_dpid = f"s{i+1}"
-            self.addSwitch(switch_dpid, stp=True, failMode='standalone')    #stp to avoid loops in the networks 
+                #for each switch create and connect all the hosts
+                for j in range(random.randrange(self.num_hosts)):
+                    host_dpid = f"h{host_count+1}"
+                    self.addHost(host_dpid)
+                    self.addLink(switch_dpid, host_dpid, bw = random.random()*(SWITCH_LINK_MAX_BW-SWITCH_LINK_MIN_BW) + SWITCH_LINK_MIN_BW )
+                    host_count +=1
 
-            if i > 0:
-                self.addLink(switch_dpid, f"s{i}")      #add link to previous switch in a line topology
+            # add connections between switches
+            connected_pairs = set()
+            for i in range(1, self.num_switches + 1):
+                for j in range(1, self.num_switches + 1):
+                    # do not link with yourself 
+                    if i == j:
+                        continue
+                    # already linked with previous router 
+                    if i == j + 1 or i == j - 1:
+                        continue
+                    # check if the reverse connection already exists
+                    if (j, i) in connected_pairs:
+                        continue
 
-            #for each switch create and connect all the hosts
-            for j in range(random.randrange(max_hosts_per_switch)):
-                host_dpid = f"h{host_count+1}"
-                self.addHost(host_dpid)
-                self.addLink(switch_dpid, host_dpid, bw = random.random()*(SWITCH_LINK_MAX_BW-SWITCH_LINK_MIN_BW) + SWITCH_LINK_MIN_BW )
-                host_count +=1
+                    if random.random() < self.interconnectivity:
+                        connected_pairs.add((i, j))  # Add the connected pair to the set
+                        self.addLink(f"s{i}", f"s{j}", bw = random.random()*(HOST_LINK_MAX_BW-HOST_LINK_MIN_BW) + HOST_LINK_MIN_BW)
     
+    def showing_topology(self):
+        G = nx.Graph()
+        
+        switch_color = 'red'
+        host_color = 'orange'
+        image_file = "topology_image.png"
+        
+        for node in self.nodes():
+            if node.startswith('s'):
+                G.add_node(node, color=switch_color, node_type='switch')
+            else:
+                G.add_node(node, color=host_color, node_type='host')
 
-        # add connections between switches
-        connected_pairs = set()
-        for i in range(1, total_switches + 1):
-            for j in range(1, total_switches + 1):
-                # do not link with yourself 
-                if i == j:
-                    continue
-                # already linked with previous router 
-                if i == j + 1 or i == j - 1:
-                    continue
-                # check if the reverse connection already exists
-                if (j, i) in connected_pairs:
-                    continue
+        for link in self.links():
+            G.add_edge(link[0], link[1])
 
-                if random.random() < interconnectivity:
-                    connected_pairs.add((i, j))  # Add the connected pair to the set
-                    self.addLink(f"s{i}", f"s{j}", bw = random.random()*(HOST_LINK_MAX_BW-HOST_LINK_MIN_BW) + HOST_LINK_MIN_BW)
+        switch_nodes = [node for node, attr in G.nodes(data=True) if attr['node_type'] == 'switch']
+        host_nodes = [node for node, attr in G.nodes(data=True) if attr['node_type'] == 'host']
+        
+        pos = nx.spring_layout(G, seed=42)
+        nx.draw_networkx_nodes(G, pos, nodelist=switch_nodes, node_color=switch_color, node_size=500, label='Switches')
+        nx.draw_networkx_nodes(G, pos, nodelist=host_nodes, node_color=host_color, node_size=300, label='Hosts')
+        nx.draw_networkx_edges(G, pos)
+        nx.draw_networkx_labels(G, pos)
+
+        plt.savefig(image_file)
+        print(f"\n*** Topology saved as '{image_file}'")
+
+class NetworkManager:
+    def __init__(self):
+        self.net = None
+
+    def cleaning_network(self):
+        info('*** Clean network instances\n')
+        Popen("mn -c", shell=True, stdout=DEVNULL, stderr=STDOUT).wait()
+
+    def connection_controller(self):
+        return RemoteController("c0", ip="0.0.0.0", port=6633) 
+    
+    def creating_net(self, topology, controller):
+        self.net = Mininet(                                                
+            topo=topology,
+            switch=OVSKernelSwitch,
+            build=False,
+            autoSetMacs=True,
+            autoStaticArp=True,
+            link=TCLink,
+            controller=controller
+        )
+        return self.net
+
+    def checking_stp_configuration(self):
+        s1 = self.net.get("s1")   #check he state stp, we wait until s1 says "forward" which indicates it is complete
+        while(s1.cmdPrint('ovs-ofctl show s1 | grep -o FORWARD | head -n1') != "FORWARD\r\n"): 
+            time.sleep(3)
+
+    def starting_servers(self):    
+        for h in self.net.hosts:
+            h.cmd('iperf -s -p 5050 &') #start iperf -Server on -Port 5050
+
+        for h in random.sample(self.net.hosts, NUM_IPERF_FLOWS):
+            hosts = self.net.hosts.copy()
+            hosts.remove(h)  #do not pick yourself
+            h.cmd(f"iperf -c -p 5050 {random.choice(hosts)} &")  #start iperf client
+            
+        for h in self.net.hosts:
+            hosts = self.net.hosts.copy()
+            hosts.remove(h)  #do not pick yourself
+            host_ips = [h.IP() for h in hosts]
+            h.cmd(f"python3 host_traffic_gen.py {' '.join(map(str, host_ips))} &")
+    
+    def creating_folder_captures(self): 
+        Popen(f"rm -rf {folder_captures}", shell=True, stdout=DEVNULL, stderr=STDOUT).wait()    #delete the folder contents before starting
+        os.mkdir(folder_captures) 
+
+    def start_switch_tcpdump(self):
+        for s in self.net.switches: #run tcpdump to capture all packets passing through every switch
+            s.cmd(f"tcpdump -w {folder_captures}/{s.name}_capture.pcap &")
+            print(f"started capturing on {s}")
+        
+    def stop_switch_tcpdump(self):
+        for s in self.net.switches:
+            s.cmd('pkill -f "tcpdump"')
 
 if __name__ == "__main__":
 
-    #clean mininet istances without print stdout
-    info('*** Clean net\n')
-    Popen("mn -c", shell=True, stdout=DEVNULL, stderr=STDOUT).wait()
+    parser = argparse.ArgumentParser(description="Network Testing Script")
+    parser.add_argument('--switches', type=int, default=7, help="Number of switches")
+    parser.add_argument('--hosts', type=int, default=2, help="Number of hosts per switch")
+    parser.add_argument('--cross-connection', type=float, default=0.30, help="Interconnectivity")
+    parser.add_argument('--time', type=int, default=30, help="Duration of the test in seconds")
+    args = parser.parse_args()
 
-    #topology creation
-    topo = arbitrary_topology(SWITCHES,HOSTS,CROSS_CONNECTION,0)
+    #Arguments
+    SWITCHES = args.switches
+    HOSTS_PER_SWITCH = args.hosts
+    CROSS_CONNECTION = args.cross_connection
+    TEST_TIME = args.time 
 
-    #logging
+    network = NetworkManager()
+    network.cleaning_network()
+
+    controller = network.connection_controller()
+
+    topology = Topology(SWITCHES, HOSTS_PER_SWITCH, CROSS_CONNECTION, 0)
+    topology.showing_topology()
+
     setLogLevel('info')
-
-    #controller
-    controller = RemoteController("c0", ip="0.0.0.0", port=6633) 
+    net = network.creating_net(topology, controller)
     
-    #mininet set-up (mac and ip auto-set)
-    net = Mininet(                                                
-        topo=topo,
-        switch=OVSKernelSwitch,
-        build=False,
-        autoSetMacs=True,
-        autoStaticArp=True,
-        link=TCLink,
-        controller=controller
-    )
-
     net.build()     
     net.start()
-    
     time.sleep(1)
     
-    dump = ""
-    for h in net.hosts:
-        dump+=f"<Host {h.name}: "
-        for intf in h.intfList():
-            dump += f"{intf.name}:{intf.IP()}"
-        dump += f" pid={h.pid}>\\n"
-
-    for s in net.switches:
-        dump +=f"<OVSSwitch {s.name}: "
-        dump += ','.join( [f"{intf.name}:{intf.IP()}" for intf in s.intfList()])
-        dump += f" pid={h.pid}>\\n"
-    links = ""
-    # Get information about links
-    for link in net.links:
-        links += f"{link.intf1.name}<->{link.intf2.name} {link.status()}\\n"
-
-    print("\n\n\nVIEW TOPOLOGY GRAPHYCALLY WITH THIS LINK:")
-    url = "http://demo.spear.narmox.com/app/?apiurl=demo#!/mininet?data="+urllib.parse.quote(f'{{"dump":"{dump}","links":"{links}"}}')
-    print(url)
+    print("\n***Network built, waiting for STP to configure itself")    
+    network.checking_stp_configuration()
     
-
-    print("\n\n--------------------------------------------------------------------------------")    
-    print("Network built, waiting for STP to configure itself\n\n")    
-
-    s1 = net.get("s1")   #check he state stp, we wait until s1 says "forward" which indicates it is complete
-    while(s1.cmdPrint('ovs-ofctl show s1 | grep -o FORWARD | head -n1') != "FORWARD\r\n"): 
-        time.sleep(3)
-    print("STP ok, waiting 5 seconds...")
+    print("\n*** STP ok, waiting 5 seconds...")
     time.sleep(5)
     
-    print("\n\n--------------------------------------------------------------------------------")    
-    print("Testing ping connectivity\n\n")    
-
+    print("\n*** Testing ping connectivity...")    
     net.pingAll()
     time.sleep(1)
 
-    
-    print("\n\n--------------------------------------------------------------------------------")    
-    print("Begin traffic generation\n\n")    
-    
+    print("\n*** Begin traffic generation\n\n")    
+    network.starting_servers()
     random.seed(time.time())
-    
-    #Start iperf (TCP and UDP) server on Hosts
-    for h in net.hosts:
-        h.cmd('iperf -s -p    5050 &') #start iperf -Server on -Port 5050
-
-    for h in random.sample(net.hosts, NUM_BASE_FLOWS):
-        hosts = net.hosts.copy()
-        hosts.remove(h)  #do not pick yourself
-        h.cmd(f"iperf -c -p 5050 {random.choice(hosts)} &")  #start iperf client
-        
-    for h in net.hosts:
-        hosts = net.hosts.copy()
-        hosts.remove(h)  #do not pick yourself
-        host_ips = [h.IP() for h in hosts]
-        h.cmd(f"python3 host_traffic_gen.py {' '.join(map(str, host_ips))} &")
-        
-    print("waiting 10s ...")
     time.sleep(10)
         
-    print("\n\n--------------------------------------------------------------------------------")    
-    print("Begin traffic capturing\n\n")    
-    
-    #empty folder 
-    Popen(f"rm -rf {folder_path}", shell=True, stdout=DEVNULL, stderr=STDOUT).wait()    #delete the folder contents before starting
-    os.mkdir(folder_path)   
+    print("\n*** Begin traffic capturing\n\n")    
+    network.creating_folder_captures()
+    network.start_switch_tcpdump()
 
-    for s in net.switches:                 #run tcpdump to capture all packets passing through every switch
-        s.cmd(f"tcpdump -w {folder_path}/{s.name}_capture.pcap &")
-        print(f"started capturing on {s}")
-   
-        
-    print(f"\n\nTest traffic started, waiting for test time ({TEST_TIME} seconds)")
+    print(f"\n*** Test traffic started, waiting for test time ({TEST_TIME} seconds)")
     time.sleep(TEST_TIME)
 
-    print("Stopping traffic capture...")
-    
-    for s in net.switches:
-        s.cmd('pkill -f "tcpdump"')
-    
-
+    print("\n*** Stopping traffic capture...")
+    network.stop_switch_tcpdump()
     net.stop()
