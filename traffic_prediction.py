de0db226d5ed3f3452562f095cf2e47223da7f97
guss-alberto <96133902+guss-alberto@users.noreply.github.com>
2024-04-16 19:50:05 +0200

    improved traffic_prediction file code

--- traffic_prediction.py
+++ traffic_prediction.py
@@ -3,59 +3,88 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 from statsmodels.tsa.arima.model import ARIMA
 from scapy.all import PcapReader
-    
+import argparse
+
 class TrafficPrediction():
-    def feature_extraction(self):
-        folder_path = "captures"
-        output_csv_path = "train"
-
-        files = os.listdir(folder_path)
-
-        for file in files:
-            print(file)
-            packets = PcapReader(folder_path+'/'+file)
-            data = []
-            for packet in packets:
-                data.append({
-                    'ds' : pd.to_datetime(float(packet.time), unit='s'),
-                    'y'  : len(packet),
-                })
-            
-            df = pd.DataFrame(data)
-            df = df.resample('.2S',on='ds').sum(numeric_only=True).fillna(0) 
-            print(df)
-            df.to_csv(output_csv_path+'/'+file+".csv")
-    
-    def run_arima(self):
-        #percentage of the input data to be used as training, the rest will just be testing data
-        training_split = .80 
-        csv_path = "train"
-        files = os.listdir(csv_path)
-
+    def feature_extraction(self, filename, output_filaname=None, sample_period='.2S'):
         data = []
-
-        for file in files:
-            df = pd.read_csv(csv_path+'/'+file, index_col='ds',parse_dates=True)
-            print(f"Fitting model for file {file}")
-            
-            training_data = df.iloc[:int(training_split*len(df))] 
-            model = ARIMA( training_data, order=(30,0,0))
+        packets = PcapReader(filename)
+        for packet in packets:
+            data.append({
+                'ds' : pd.to_datetime(float(packet.time), unit='s'),
+                'y'  : len(packet),
+            })
+        
+        self.df = pd.DataFrame(data)
+        self.df = self.df.resample(sample_period,on='ds').sum(numeric_only=True).fillna(0) 
+        
+        if output_filaname:
+            self.df.to_csv(output_filaname)
+    
+    
+    def read_from_csv(self, filename):
+        self.df = pd.read_csv(filename, index_col='ds',parse_dates=True)
+    
+    def run_arima(self, order, training_split=.8):
+        #percentage of the input data to be used as training, the rest will just be testing data           
+            self.training_data = self.df.iloc[:int(training_split*len(self.df))] 
+            model = ARIMA( self.training_data, order=order)
             fitted_model = model.fit()
             
-            print("model fitting done")
-            print(fitted_model.summary())
+            # print("model fitting done")
+            # print(fitted_model.summary())
 
-            prediction = fitted_model.predict(start=df.index.min(),end=df.index.max())
-            plt.plot(prediction, label="prediction")
-            plt.plot(df, label="data")
-            plt.gca().axvline(x=training_data.index.max(), color='red', linestyle='--', label='Training Split')  
-            plt.gca().legend()
-            plt.show()
-
+            self.prediction = fitted_model.predict(start=self.df.index.min(),end=self.df.index.max())
+            
+    def plot(self, title, output_file):
+        ax = plt.gca()
+        plt.plot(self.prediction, label="prediction")
+        plt.plot(self.df, label="data")
+        ax.axvline(x=self.training_data.index.max(), color='red', linestyle='--', label='Training Split')  
+        ax.legend()
+        plt.title(title)
+        plt.savefig(output_file)
+        plt.show()
+        
 
 if __name__ == "__main__":
-    prediction = TrafficPrediction()
+    parser = argparse.ArgumentParser(description="Network traffic prediction script")
+    parser.add_argument('--pcap-path', type=str, default="captures", help="Folder containing the .pcap files to use as input")
+    parser.add_argument('--csv', type=str, help="Folder containing the csv files to use as input (will ignore pcap-path and sample-period)")
+    parser.add_argument('--store-csv', type=str, help="Folder where to store the training data in csv files")
+    parser.add_argument('--store-plot', type=str, default="plots", help="Folder where to store the plots")
+    parser.add_argument('--training-split', type=float, default=.8, help="Percentage of data used for training")
+    parser.add_argument('--sample-period', type=str, default=".2S", help="Preiod over which to combine network data")
+    
+    args = parser.parse_args()
+    
+    if args.csv:
+        path = args.csv
+        files = [file  for file in os.listdir(path) if file.endswith('.csv')]
+    else:
+        path = args.pcap_path
+        files = [file  for file in os.listdir(path) if file.endswith('.pcap')]
+    
+    for file in files:
+        
+        prediction = TrafficPrediction()
+        
+        full_path = os.path.join(path, file)
+        print(f"Reading file {full_path}")
+        if args.csv:
+            prediction.read_from_csv(full_path)
+        else:
+            prediction.feature_extraction(full_path, output_filaname=args.store_csv, sample_period=args.sample_period)
+        
+        print("Running ARIMA prediction")
+        prediction.run_arima(order=(30,0,0), training_split=args.training_split)
+        prediction.plot(file, os.path.join(args.store_plot, file+'.png'))
+        
+    
+    
+    args = parser.parse_args()
+    
     prediction.feature_extraction()
-    prediction.run_arima()
+    
 
 
